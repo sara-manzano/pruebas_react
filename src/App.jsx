17da84c47388368bfca7f48db80f09c792b1d0fc
Sara <sara@quesu.local>
2026-07-07 12:39:47 +0200

    MyComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 
+const MiComponente = () => {
+  const nombre = "Usuario";
+  return (
+    <div>
+      <h2>Hola {nombre}</h2>
+      <p>Bienvenido a la aplicación</p>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <h1>Pruebas React</h1>
+      <MiComponente />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
